eeb003f3a73a3016a1a185afab042cd7cf7c06eb
leonbagdag <dsrt1991@gmail.com>
2019-11-22 18:16:06 +0000

    Home

---
+++ src/js/component/presentation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export const Presentation = () => {
+	return (
+		<div className="homeV2_profsWrapper">
+			<h3>HOLA</h3>
+		</div>
+	);
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+		<div className="col-md-5 p-lg-5 mx-auto my-5">
+			<h1 className="display-4 font-weight-normal">DoItForMe</h1>
+			<p className="lead font-weight-normal">Imagen sobre que hace y para que se usa</p>
+		</div>
+		<div className="product-device shadow-sm d-none d-md-block"></div>
+		<div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
 	</div>
 );
