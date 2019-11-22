301394a6e227aaccdec2b111a781fed3cbb7b982
leonbagdag <dsrt1991@gmail.com>
2019-11-22 18:24:20 +0000

    search component

---
+++ src/js/component/Search.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export const Presentation = () => {
+	return (
+		<div className="Container">
+            <div className="Row">
+            <div className="col-md-6 col-md-offset-3">
+             <h1>Expandable Search Form</h1>
+            </div>
+            </div>
+              <div className="row">
+              <div class="col-md-4 col-md-offset-3">
+              </div>
+              </div>
+			<h3>HOLA</h3>
+		</div>
+	);
+};
