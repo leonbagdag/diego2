f086e0f7815b35cd08b6390f23c3336428f98b32
leonbagdag <dsrt1991@gmail.com>
2019-11-29 17:57:17 +0000

    vistas home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -32,9 +32,22 @@ export const Home = () => (
 				</Carousel.Item>
 			</Carousel>
 		</div>
+		{/*el cuadro de busqueda debe ser un componente, lo dejo como vista pero falta desarrollo*/}
 		<div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
 			<div className="col-md-5 p-lg-5 mx-auto my-5">
-				<h1 className="display-4 font-weight-normal">SEARCH</h1>
+				<h1 className="display-4 font-weight-normal">¿Buscas algun Servicio?</h1>
+				<div className="input-group md-form form-sm form-2 pl-0">
+					<input
+						className="form-control my-0 py-1 red-border"
+						type="text"
+						placeholder="Search"
+						aria-label="Search"></input>
+					<div className="input-group-append">
+						<span className="input-group-text red lighten-3" id="basic-text1">
+							<i className="fas fa-search text-grey" aria-hidden="true"></i>
+						</span>
+					</div>
+				</div>
 			</div>
 			<div className="product-device shadow-sm d-none d-md-block"></div>
 			<div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
@@ -55,30 +68,3 @@ export const Home = () => (
 		</div>
 	</body>
 );
-
-/*<div className="Carouse">
-			<Carousel>
-				<Carousel.Item>
-					<img className="max-width: 110%" src="https://picsum.photos/id/1/1200/500" alt="First slide" />
-					<Carousel.Caption>
-						<h3>First slide label</h3>
-						<p>Que es lo que hace DoItForMe</p>
-					</Carousel.Caption>
-				</Carousel.Item>
-				<Carousel.Item>
-					<img className="max-width: 110%" src="https://picsum.photos/id/1/1200/500" alt="Third slide" />
-
-					<Carousel.Caption>
-						<h3>Second slide label</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-					</Carousel.Caption>
-				</Carousel.Item>
-				<Carousel.Item>
-					<img className="max-width: 110%" src="https://picsum.photos/id/1/1200/500" alt="Third slide" />
-
-					<Carousel.Caption>
-						<h3>Third slide label</h3>
-						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-					</Carousel.Caption>
-				</Carousel.Item>
-			</Carousel>*/
