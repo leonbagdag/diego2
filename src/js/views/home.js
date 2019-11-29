ab6323f1bef7799f253ba6b6fe1d978ecb6f8325
leonbagdag <dsrt1991@gmail.com>
2019-11-29 17:10:11 +0000

    secciones home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -5,39 +5,32 @@ import Carousel from "react-bootstrap/Carousel";
 
 export const Home = () => (
 	<body>
-		<div className="Carouse">
+		<div div id="myCarousel" className="carousel slide" data-ride="carousel">
 			<Carousel>
 				<Carousel.Item>
-					<img className="max-width: 110%" src="https://picsum.photos/id/1/1200/500" alt="First slide" />
+					<img className="max-width: 110%" src="https://picsum.photos/id/1/1280/500" alt="First slide" />
 					<Carousel.Caption>
-						<h3>First slide label</h3>
-						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+						<h3>DoItForMe</h3>
+						<p>Que es lo que hace DoItForMe</p>
 					</Carousel.Caption>
 				</Carousel.Item>
 				<Carousel.Item>
-					<img className="max-width: 110%" src="https://picsum.photos/id/1/1200/500" alt="Third slide" />
+					<img className="max-width: 110%" src="https://picsum.photos/id/1/1280/500" alt="Third slide" />
 
 					<Carousel.Caption>
-						<h3>Second slide label</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<h3>DoItForMe</h3>
+						<p>Que es los que hace DoItForMe</p>
 					</Carousel.Caption>
 				</Carousel.Item>
 				<Carousel.Item>
-					<img className="max-width: 110%" src="https://picsum.photos/id/1/1200/500" alt="Third slide" />
+					<img className="max-width: 110%" src="https://picsum.photos/id/1/1280/500" alt="Third slide" />
 
 					<Carousel.Caption>
-						<h3>Third slide label</h3>
-						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+						<h3>DoItForMe</h3>
+						<p>Que es lo que hace DoItForMe</p>
 					</Carousel.Caption>
 				</Carousel.Item>
 			</Carousel>
-		</div>
-		<div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-			<div className="col-md-5 p-lg-5 mx-auto my-5">
-				<h1 className="display-4 font-weight-normal">STATISTICS</h1>
-			</div>
-			<div className="product-device shadow-sm d-none d-md-block"></div>
-			<div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
 		</div>
 		<div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
 			<div className="col-md-5 p-lg-5 mx-auto my-5">
@@ -62,3 +55,30 @@ export const Home = () => (
 		</div>
 	</body>
 );
+
+/*<div className="Carouse">
+			<Carousel>
+				<Carousel.Item>
+					<img className="max-width: 110%" src="https://picsum.photos/id/1/1200/500" alt="First slide" />
+					<Carousel.Caption>
+						<h3>First slide label</h3>
+						<p>Que es lo que hace DoItForMe</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img className="max-width: 110%" src="https://picsum.photos/id/1/1200/500" alt="Third slide" />
+
+					<Carousel.Caption>
+						<h3>Second slide label</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img className="max-width: 110%" src="https://picsum.photos/id/1/1200/500" alt="Third slide" />
+
+					<Carousel.Caption>
+						<h3>Third slide label</h3>
+						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>*/
