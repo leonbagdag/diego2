be8811742cd1bcff5fe1784f122f8e440fdd7dba
Diego Rojas <dsrt1991@gmail.com>
2020-02-10 20:14:07 -0300

    cambios flux y panel de control

--- src/js/component/login.js
+++ src/js/component/login.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 
 class Form extends React.Component {
@@ -28,9 +29,23 @@ class Form extends React.Component {
 			fields["contraseña"] = "";
 			this.setState({ fields: fields });
 			alert("Form submitted");
+=======
+import React, { useState, useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		"& > *": {
+			margin: theme.spacing(1),
+			width: 200
+>>>>>>> Stashed changes
 		}
 	}
+}));
 
+<<<<<<< Updated upstream
 	validateForm() {
 		let fields = this.state.fields;
 		let errors = {};
@@ -99,3 +114,35 @@ class Form extends React.Component {
 }
 
 export default Form;
+=======
+const SignIn = () => {
+	const [user, setUser] = useState({
+		email: "",
+		password: ""
+	});
+	const inputChange = event => {
+		setUser({ ...user, [event.target.name]: event.target.value });
+	};
+	const signIn = () => {
+		// validate al data
+		// call back-end wa adelo al user value
+	};
+	return (
+		<div>
+			<h1>Online Shop</h1>
+			<img
+				url="https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/06/shopping-online.jpg"
+				alt="cart"></img>
+			<h3>Sign in</h3>
+			<TextField name="email" id="outlined-basic" label="Outlined" variant="outlined" onChange={inputChange} />
+			<TextField name="password" id="outlined-basic" label="Outlined" variant="outlined" onChange={inputChange} />
+			<Button variant="contained" color="primary" onClick={signIn}>
+				Primary
+			</Button>
+			<a href="">New Customer? Sign up!</a>
+		</div>
+	);
+};
+
+export default SignIn;
+>>>>>>> Stashed changes
--- src/js/views/loginform.js
+++ src/js/views/loginform.js
@@ -1,4 +1,5 @@
 import React from "react";
+<<<<<<< Updated upstream
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBInput } from "mdbreact";
 import Form from "../component/login";
 import "../../styles/loginform.scss";
@@ -10,3 +11,12 @@ export const Loginform = () => {
 		</div>
 	);
 };
+=======
+import Header from "../component/login";
+
+export const Loginform = () => (
+	<div className="container">
+		<Login />
+	</div>
+);
+>>>>>>> Stashed changes
